25c5bdc281678eb574a37a09c55518673d021332
Inooid <justdrawcats@gmail.com>
2014-11-18 20:34:32 +0100

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name boyddamesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the boyddamesApp
+ */
+angular.module('boyddamesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name boyddamesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the boyddamesApp
+ */
+angular.module('boyddamesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
